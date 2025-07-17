857aa694d9bf81b0c4c2aaff5125bcbf597185e2
Himu <himavarshithparepalli@gmail.com>
2025-07-18 00:01:38 +0530

    week_4

---
+++ week4/Hello World RESTful Web Service/spring-learn/src/test/java/com/cognizant/spring_learn/SpringLearnApplicationTests.java
@@ -0,0 +1,11 @@
+package com.cognizant.spring_learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+@SpringBootTest(classes = com.cognizant.spring_learn.SpringLearnApplication.class)
+class SpringLearnApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
+
